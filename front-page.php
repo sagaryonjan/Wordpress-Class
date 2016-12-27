3673f66a2e49e12d715dcc05273a47ca2368bbcc
Dipendra K.C <Dipendra K.C>
2016-12-26 19:22:08 -0800

    Some thing changed

--- front-page.php
+++ front-page.php
@@ -16,7 +16,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		this is test
+		this is test hjdkghsdkg
 
 		<?php
 		if ( have_posts() ) :
