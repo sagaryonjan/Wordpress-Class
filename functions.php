731360fa9b440e81d693c7ce8b1780b58ba8e941
sagaryonjan <sagaryonjan015@gmail.com>
2016-12-27 10:01:45 +0545

    change

--- functions.php
+++ functions.php
@@ -83,23 +83,9 @@ function wordpress_class_content_width() {
 }
 add_action( 'after_setup_theme', 'wordpress_class_content_width', 0 );
 
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function wordpress_class_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'wordpress_class' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'wordpress_class' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'wordpress_class_widgets_init' );
+
+
+
 
 /**
  * Enqueue scripts and styles.
@@ -141,3 +127,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Wordpress Related Register widget area.
+ */
+require get_template_directory() . '/inc/wordpress_class_function.php';
---
+++ inc/wordpress_class_function.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function wordpress_class_widgets_init() {
+
+    register_sidebar( array(
+        'name'          => esc_html__( 'Sidebar', 'wordpress_class' ),
+        'id'            => 'sidebar-1',
+        'description'   => esc_html__( 'Add widgets here.', 'wordpress_class' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => esc_html__( 'Front Page Feature Post', 'wordpress_class' ),
+        'id'            => 'feature_post',
+        'description'   => esc_html__( 'Add widgets which is suitable for feature post.', 'wordpress_class' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
+}
+add_action( 'widgets_init', 'wordpress_class_widgets_init' );
+
+
+
+
+
+
+
